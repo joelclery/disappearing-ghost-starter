feaa2e725cec6de98db2dc7c76cc1291d321fa98
Glitch (admitted-fragrant-viscountess) <none>
2020-11-19 23:33:29 +0000

    👏🖼 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
+$(".update-img-button").click(function() {
+  $(".ghost-image").css("https://ih1.redbubble.net/image.153656513.6605/flat,800x800,075,f.jpg");
+});
